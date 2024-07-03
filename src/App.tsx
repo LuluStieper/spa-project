23b79d849d7f16bd7497185f1ef95090002c539e
Your Name <you@example.com>
2024-07-03 11:23:32 +0200

    React Router aktiviert

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,36 @@ import { ProtokollResource } from './Resources';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './components/ErrorFallback';
 import { AlleProtokolle } from './components/AlleProtokolle';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PageIndex } from './components/PageIndex';
+import { PageProtokoll } from './components/PageProtokoll';
+import { PageEintrag } from './components/PageEintrag';
+import { PageAdmin } from './components/PageAdmin';
+import { PagePrefs } from './components/PagePrefs';
+import React from 'react';
 
 function App() {
 
   const [selectedProtokoll, setSelectedProtokoll] = useState<ProtokollResource | null>(null);
 
+
+
+
   return (
-    <>
+    <React.StrictMode>
+    <BrowserRouter>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <h1>Trinkprotokolle</h1>
-        <AlleProtokolle></AlleProtokolle>
+      <Routes>
+        <Route path="/" element={<PageIndex />}/>
+        <Route path="/protokoll/:protokollId" element={<PageProtokoll />}/>
+        <Route path="/eintrag/:eintragId"element={<PageEintrag />}/>
+        <Route path="/admin" element={<PageAdmin />}/>
+        <Route path="/prefs" element={<PagePrefs />}/>
+        <Route path="*" element={<PageIndex />} />
+      </Routes>
       </ErrorBoundary>
-    </>
+    </BrowserRouter>
+  </React.StrictMode>
   );
 
 }
